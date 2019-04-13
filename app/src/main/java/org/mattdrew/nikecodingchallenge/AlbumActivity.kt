e51d75dae1580a1785858dbdebb6abcc8c448d80
moshulu <mdrew205@gmail.com>
2019-04-12 23:06:04 -0400

    0.0.0

---
+++ app/src/main/java/org/mattdrew/nikecodingchallenge/AlbumActivity.kt
@@ -0,0 +1,12 @@
+package org.mattdrew.nikecodingchallenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+    }
+}
